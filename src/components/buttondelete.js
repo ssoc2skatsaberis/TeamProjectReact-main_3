3fefa5bd91ed115a1737713877eca8eaee155400
ssoc2skatsaberis <118774780+ssoc2skatsaberis@users.noreply.github.com>
2023-03-05 12:30:05 +0200

    Delete Button Done
    delete button works and refresh and redirent to course page

--- src/components/buttondelete.js
+++ src/components/buttondelete.js
@@ -1,17 +1,26 @@
 
 import axios from 'axios';  
-    
+import { useNavigate } from "react-router-dom";    
 
 export default function ButtonDelete(props){
-  
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+        let path = `/courses`; 
+        navigate(path);
+      }
     function deleteUser(id){
         var answer = window.confirm("Delete data?");
         if (answer) {
             let link = 'http://localhost:3001/courses/'+id;
-            console.log("Try delete",answer, link);
-            console.log(typeof link);
+            //console.log("Try delete",answer, link);
+            //console.log(typeof link);
             axios.delete(link)
-                .then(response => console.log( 'Delete successful'))
+                .then(response => {
+                    console.log( 'Delete successful')
+                    routeChange();
+                    
+                }
+                    )
                 .catch(error => {
                     console.error('There was an error!', error);
             });
